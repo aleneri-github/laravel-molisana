1a4812b18ced7639241b5eff2659be5208139926
Alessandro <aleneri242@gmail.com>
2021-02-03 19:57:52 +0100

    aggiornamento footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,7 +55,24 @@
         </div>
       </main>
       <footer>
-        <img src="{{asset("img/footer-montagne-bottom.jpg")}}" alt="Montagne">
+        <div class="footer-top">
+          <div class="container80">
+          <section>
+            <img src="{{asset("img/marchio-sito-test.png")}}" alt="La Molisana">
+          </section>
+          <section>
+          </section>
+          <section>
+          </section>
+
+
+          </div>
+
+        </div>
+        <div class="footer-bottom">
+          <img src="{{asset("img/footer-montagne-bottom.jpg")}}" alt="Montagne">
+
+        </div>
       </footer>
 
     </body>
