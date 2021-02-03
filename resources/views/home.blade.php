0c45181e04ff1e4be81947495dddb419bac339a8
Alessandro <aleneri242@gmail.com>
2021-02-03 17:47:50 +0100

    inserito array

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,24 +5,26 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Molisana</title>
         <!-- CSS -->
-        <link rel="stylesheet" href="{{ asset("css/app.css")}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
       <header>
         <img src="{{asset("img/marchio-sito-test.png")}}" alt="La Molisana">
         <ul class="list-inline">
           <li>
-            <a href="#">HOME</a>
+            <a href="#">Home</a>
           </li>
           <li>
-            <a href="#">PRODOTTI</a>
+            <a href="#">Prodotti</a>
           </li>
           <li>
-            <a href="#">NEWS</a>
+            <a href="#">News</a>
           </li>
-
         </ul>
       </header>
+      <main>
+
+      </main>
 
     </body>
 </html>
