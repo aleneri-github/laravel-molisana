6fffb588087a532f4c798e6bdb3f9bfa6b6441b2
Alessandro <aleneri242@gmail.com>
2021-02-03 16:56:38 +0100

    inizio header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Molisana</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- CSS -->
         <link rel="stylesheet" href="{{ asset("css/app.css")}}">
-
-
-
     </head>
     <body>
+      <header>
+        <img src="{{asset("img/marchio-sito-test.png")}}" alt="La Molisana">
+        <ul class="list-inline">
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">PRODOTTI</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+
+        </ul>
+      </header>
 
     </body>
 </html>
