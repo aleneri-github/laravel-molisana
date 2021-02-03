4600ca7c09d84a7eb25d38f96d4abb726f61fa99
Alessandro <aleneri242@gmail.com>
2021-02-03 18:54:53 +0100

    aggiornamento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,36 @@
         </ul>
       </header>
       <main>
+        <div class="container80">
+          <h2>LE LUNGHE</h2>
+          <div class="items">
+            @foreach ($lunghe as $pasta)
+            <div class="card">
+              <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+            </div>
+            @endforeach
+          </div>
+        </div>
+        <div class="container80">
+          <h2>LE CORTE</h2>
+          <div class="items">
+            @foreach ($corte as $pasta)
+            <div class="card">
+              <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+            </div>
+            @endforeach
+          </div>
+        </div>
+        <div class="container80">
+          <h2>LE CORTISSIME</h2>
+          <div class="items">
+            @foreach ($cortissime as $pasta)
+            <div class="card">
+              <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+            </div>
+            @endforeach
+          </div>
+        </div>
 
       </main>
 
