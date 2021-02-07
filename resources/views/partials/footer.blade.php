7001a61d6bd8eaf2cb9b4c4f8e437742067aeab8
Alessandro <aleneri242@gmail.com>
2021-02-07 10:24:43 +0100

    nuovo aggiornamento footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -18,10 +18,41 @@
             </p>
 
       </section>
+
       <section>
+        <h4>PASTIFICIO</h4>
+        <ul>
+          <li><a href="#">Il pastificio</a> </li>
+          <li><a href="#">Grano decorticato a pietra</a> </li>
+          <li><a href="#">Il Molise c'è</a> </li>
+          <li><a href="#">Filiera integrata</a> </li>
+          <li><a href="#">100 anni di pasta</a> </li>
+          <li><a href="#">Sartoria della pasta</a> </li>
+          <li><a href="#">Spagetto quadrato</a> </li>
+          <li><a href="#">Le Persone</a> </li>
+        </ul>
+        <h4>PRODOTTI</h4>
+        <ul>
+          <li><a href="#">Le Classiche</a> </li>
+          <li><a href="#">Le Integrali</a> </li>
+          <li><a href="#">Le Speciali</a> </li>
+          <li><a href="#">Le Biologiche</a> </li>
+          <li><a href="#">Le Gluten-Free</a> </li>
+          <li><a href="#">Le Semole</a> </li>
+          <li><a href="#">Le Extra di Lusso</a> </li>
+
+        </ul>
 
       </section>
       <section>
+        <h4>COLLEZIONE DA CHEF</h4>
+        <ul>
+          <li><a href="#">Collezione da Chef</a> </li>
+          <li><a href="#">Grandi Cucine</a> </li>
+          <li><a href="#">Biologiche</a> </li>
+          <li><a href="#">Quadrate</a> </li>
+        </ul>
+
 
       </section>
 
