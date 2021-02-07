02cbe31f5614ef042a165c7c0b1a5bbc8b7567d0
Alessandro <aleneri242@gmail.com>
2021-02-07 10:13:06 +0100

    nuovo aggiornamento

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,13 +3,26 @@
     <div class="container80 clearfix">
       <section>
           <img src="{{asset("img/marchio-sito-test.png")}}" alt="La Molisana">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <p>
+                <strong>RAGIONE SOCIALE</strong>: LA MOLISANA S.P.A.<br>
+                <strong>SEDE LEGALE</strong>: CONTRADA COLLE DELLE API, 100/A - 86100 - CAMPOBASSO (CB)<br>
+                <strong>PEC</strong>: LAMOLISANA@PEC.IT<br>
+                <strong>TEL</strong>: +39 0874 4981<br>
+                <strong>FAX</strong>: +39 0874 629584<br>
+                PER SEGNALAZIONI SCRIVERE A:<br>
+                <strong>INFO@LAMOLISANA.IT</strong><br>
+                PER CONTATTARE L'AREA COMMERCIALE:<br>
+                <strong>COMMERCIALE@LAMOLISANA.IT</strong><br>
+                PER CONTATTARE L'AREA EXPORT:<br>
+                <strong>EXPORT@LAMOLISANA.IT</strong><br>
+            </p>
+
       </section>
       <section>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
       </section>
       <section>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
       </section>
 
 
